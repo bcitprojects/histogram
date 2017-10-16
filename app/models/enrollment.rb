1ef0512dbb41b43a5cb6c1472001eea96e1574e4
kevin-le111@hotmail.com <lekevinl@sfu.ca>
2017-10-15 20:44:59 -0700

    Add active record validations

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,4 +1,7 @@
 class Enrollment < ActiveRecord::Base
   belongs_to :student
   belongs_to :course
+  validates  :student_id, presence: true
+  validates  :course_id, presence: true
+  validates  :percentage, numericality: {greater_than_or_equal_to: 0.0, less_than_or_equal_to: 100.00}
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,5 @@
 class Student < ActiveRecord::Base
   has_many :enrollments
+  validates :name, length: { minimum: 2 }, presence: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, presence: true
 end
